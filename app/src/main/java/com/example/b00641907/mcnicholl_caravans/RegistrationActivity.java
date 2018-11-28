4dfe8e664897b7f70e3ae5b30a644f392eebb032
Eoghan Wilkinson <Wilkinson-E4email.ulster.ac.uk>
2018-11-28 12:58:29 +0000

    Connecting to firebase

--- app/src/main/java/com/example/b00641907/mcnicholl_caravans/RegistrationActivity.java
+++ app/src/main/java/com/example/b00641907/mcnicholl_caravans/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.b00641907.mcnicholl_caravans;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +10,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText userName, userPassword, userEmail;
     private Button regButton;
     private TextView userLogIn;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -22,11 +29,29 @@ public class RegistrationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         setupUIViews();
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (validate()) {
                     //Upload data to the database
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_password = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password) .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                Toast.makeText(RegistrationActivity.this, "You have Registered Successfully", Toast.LENGTH_SHORT) .show();
+                                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(RegistrationActivity.this, "You Failed to Register", Toast.LENGTH_SHORT) .show();
+                            }
+
+                        }
+                    });
                 }
             }
         });
@@ -55,7 +80,7 @@ public class RegistrationActivity extends AppCompatActivity {
         String password = userPassword.getText().toString();
         String email = userEmail.getText().toString();
 
-        if (name.isEmpty() && password.isEmpty() && email.isEmpty()) {
+        if (name.isEmpty() || password.isEmpty() || email.isEmpty()) {
             Toast.makeText(this, "Please enter all details", Toast.LENGTH_SHORT).show();
         } else {
             result = true;

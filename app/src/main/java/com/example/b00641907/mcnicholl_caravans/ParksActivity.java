b88f73be28c4e754c1d6690f978a218473a40784
Eoghan Wilkinson <Wilkinson-E4email.ulster.ac.uk>
2018-11-29 21:56:43 +0000

    Sign out working and start of page links

---
+++ app/src/main/java/com/example/b00641907/mcnicholl_caravans/ParksActivity.java
@@ -0,0 +1,24 @@
+package com.example.b00641907.mcnicholl_caravans;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import static android.os.Build.VERSION_CODES.P;
+
+public class ParksActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parks);
+
+    }
+
+}
+
--- app/src/main/java/com/example/b00641907/mcnicholl_caravans/SecondActivity.java
+++ app/src/main/java/com/example/b00641907/mcnicholl_caravans/SecondActivity.java
@@ -1,13 +1,71 @@
 package com.example.b00641907.mcnicholl_caravans;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button signout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signout = (Button)findViewById(R.id.btnSignOut);
+
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Signout();
+
+            }
+        });
+
+
     }
+
+    private void Signout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    private void Parks(){
+
+        startActivity(new Intent(SecondActivity.this, ParksActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.SignOutMenu:
+                Signout();
+                return true;
+            case R.id.ParksMenu:
+                Parks();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
